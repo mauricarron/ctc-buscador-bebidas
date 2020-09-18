de16d014aa1e0f3c5c8d3f3b18d2dce8fe06b90d
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-18 16:25:04 -0300

    Completa 9. Creando un segundo Context

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,11 @@
 import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriasContext";
+import { RecetasContext } from "../context/RecetasContext";
 
 const Formulario = () => {
   const { categorias } = useContext(CategoriasContext);
+  const { buscarRecetas } = useContext(RecetasContext);
+
   const [busqueda, guardarBusqueda] = useState({
     nombre: "",
     categoria: "",
@@ -16,7 +19,13 @@ const Formulario = () => {
   };
 
   return (
-    <form className="col-12">
+    <form
+      className="col-12"
+      onSubmit={(e) => {
+        e.preventDefault();
+        buscarRecetas(busqueda);
+      }}
+    >
       <fieldset className="text-center">
         <legend>Busca bebidas por categoría o ingrediente</legend>
       </fieldset>
