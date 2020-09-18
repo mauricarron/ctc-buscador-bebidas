10986f459775455dad015580bdbd51d5c2c92e62
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-18 12:36:25 -0300

    Completa 6. Consultando la API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
+  const { categorias } = useContext(CategoriasContext);
+  console.log(categorias);
   return (
     <form className="col-12">
       <fieldset className="text-center">
